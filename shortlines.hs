675f43ca92b5bde6f453ddd73d4a719c800ebb63
Pai <nsrikote@mail.uni-paderborn.de>
2024-06-10 01:59:23 +0700

    :recycle: optimize readability

--- shortlines.hs
+++ shortlines.hs
@@ -1,7 +1,1 @@
-main = interact shortLinesOnly
-
-shortLinesOnly :: String -> String
-shortLinesOnly input =
-  let allLines = lines input
-      shortLines = filter (\l -> length l < 10) allLines
-   in unlines shortLines
+main = interact $ unlines . filter ((< 10) . length) . lines
